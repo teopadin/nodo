2fdbe00219ff110e2ba6e492f689ee02da5a2536
Teo Padin <teopadin3@gmail.com>
2026-04-16 15:51:08 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Nodo", layout="centered")
+
+st.title("Nodo")
+st.markdown("---")
+
+st.subheader("Configuración en curso...")
+st.write("Si ves esto, tu web minimalista ya está funcionando.")
